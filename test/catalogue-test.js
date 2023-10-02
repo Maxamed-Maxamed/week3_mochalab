6c9750e5370c7dc58ece2631ed5f5d23489235a4
Maxamed Maxamed <20088349@mail.wit.ie>
2023-10-02 19:11:16 +0100

    Tested remove and find a product by id methods.

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -6,6 +6,8 @@ const expect = chai.expect;
 let cat = null;
 let batch = null;
 
+/* The `describe` block is used to group related test cases together. In this case, it is grouping test
+cases related to the "Catalogue" class. */
 describe("Catalogue", () => {
   beforeEach(() => {
     console.log("before block");
@@ -14,16 +16,23 @@ describe("Catalogue", () => {
     cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
     cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
   });
+  
+  /* The code you provided is a test case for the `findProductById` method of the `Catalogue` class. */
   describe("findProductById", function () {
     it("should find a valid product id", function () {
       const result = cat.findProductById("A123");
       expect(result.name).to.equal("Product 1");
     });
+
+    /* The code you provided is a test case for the `findProductById` method of the `Catalogue` class. */
     it("should return undefined for invalid product id", function () {
       const result = cat.findProductById("A321");
       expect(result).to.be.undefined;
     });
   });
+
+  /* The code you provided is a test case for the 
+  `removeProductById` method of the `Catalogue` class. */
   describe("removeProductById", () => {
     it("should remove product with a valid id", () => {
       let result = cat.removeProductById("A123");
